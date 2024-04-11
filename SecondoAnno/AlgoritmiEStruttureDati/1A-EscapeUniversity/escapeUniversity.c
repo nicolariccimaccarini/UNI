d580295d6b4b297d5e30d3e4a7fcaa1d1df14993
nicolariccimaccarini <115147017+nicolariccimaccarini@users.noreply.github.com>
2024-04-11 22:06:33 +0200

    update escapeUniversity

--- SecondoAnno/AlgoritmiEStruttureDati/1A-EscapeUniversity/escapeUniversity.c
+++ SecondoAnno/AlgoritmiEStruttureDati/1A-EscapeUniversity/escapeUniversity.c
@@ -8,89 +8,63 @@ typedef struct {
     int val;
 } valu;
 
-void merge(valu arr[], int left, int mid, int right) {
-    int i, j, k;
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
+void merge(valu *values, int start, int mid, int end) {
+    int n1 = mid - start + 1;
+    int n2 = end - mid;
 
-    valu L[n1], R[n2];
+    int i, j;
+    valu *left = malloc(n1 * sizeof(valu));
+    valu *right = malloc(n2 * sizeof(valu));
 
-    for (i = 0; i < n1; i++)
-        L[i] = arr[left + i];
-    for (j = 0; j < n2; j++)
-        R[j] = arr[mid + 1 + j];
+    for (i=0; i<n1; i++) 
+        left[i] = values[start+i];
+    for (j=0; j<n2; j++) 
+        right[j] = values[mid+1+j];
+    
+    i = j = 0;
 
-    i = 0;
-    j = 0;
-    k = left;
-
-    while (i < n1 && j < n2) {
-        if (L[i].val <= R[j].val) {
-            arr[k] = L[i];
-            i++;
+    for (int k=start; k<=end; k++) {
+        if (i < n1) {
+            if (j < n2) {
+                values[k] = (left[i].val <= right[j].val) ? left[i++] : right[j++];
+            } else {
+                values[k] = left[i++];
+            }
         } else {
-            arr[k] = R[j];
-            j++;
+            values[k] = right[j++];
         }
-        k++;
     }
 
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
+    free(left);
+    free(right);
+}
 
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
+void mergeSort(valu *values, int start, int end) {
+    if (start < end) {
+        int mid = start + (end-start) / 2;
+
+        mergeSort(values, start, mid);
+        mergeSort(values, mid+1, end);
+
+        merge(values, start, mid, end);
     }
 }
 
-void mergeSort(valu arr[], int left, int right) {
-    if (left < right) {
-        int mid = left + (right - left) / 2;
+valu find(valu *values, int low, size_t values_size, int target) {
+    if (low > values_size)
+        return values[0];
 
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-
-        merge(arr, left, mid, right);
-    }
+    int mid = (low + values_size) / 2;
+    if (values[mid].val == target)
+        return values[mid];
+    if (values[mid].val < target)
+        return find(values, mid+1, values_size, target);
+    if (values[mid].val > target)
+        return find(values, low, mid-1, target);
 }
-
-int binarySearch(valu arr[], int left, int right, int target) {
-    if (right >= left) {
-        int mid = left + (right - left) / 2;
-
-        if (arr[mid].val == target) {
-            return mid;
-        }
-
-        if (arr[mid].val > target) {
-            return binarySearch(arr, left, mid - 1, target);
-        }
-
-        return binarySearch(arr, mid + 1, right, target);
-    }
-
-    return -1; // target not found
-}
-
-valu find(valu values[], size_t values_size, int target) {
-    int index = binarySearch(values, 0, values_size - 1, target);
-
-    if (index != -1) {
-        return values[index];
-    }
-
-    valu null_val = {0, 0}; // create a null value
-    return null_val;
-}
-
 
 void solve(valu *values, int values_size, int query, FILE *out_file) {
-    valu p = find(values, values_size, query);
+    valu p = find(values, 0, values_size, query);
 
     if (p.val == query) {
         fprintf(out_file, "%d ", p.id);
@@ -100,22 +74,22 @@ void solve(valu *values, int values_size, int query, FILE *out_file) {
 }
 
 void escape_university(FILE *in_file, FILE *out_file) {
-    int N, Q;
-    fscanf(in_file, "%d %d", &N, &Q);
-
     valu values[MAX_SIZE];
     int values_size = 0;
+    
+    int N, Q;
+    fscanf(in_file, "%d %d", &N, &Q);
 
     for (int i=0; i<N; i++) {
         int id, s;
         fscanf(in_file, "%d %d", &id, &s);
 
         for (int j=0; j<s-1; j++) {
-            int x; 
-            fscanf(in_file, "%d", &x);
+            int val; 
+            fscanf(in_file, "%d", &val);
 
             values[values_size].id = id;
-            values[values_size].val = x;
+            values[values_size].val = val;
             values_size++;
         }
     }
@@ -127,10 +101,9 @@ void escape_university(FILE *in_file, FILE *out_file) {
         fscanf(in_file, "%d", &query);
         solve(values, values_size, query, out_file);
     }
-    
 }
 
-int main(void) {
+int main() {
     // Apro i file di input e output
     FILE *in_file = fopen("input.txt", "r");
     FILE *out_file = fopen("output.txt", "w");
