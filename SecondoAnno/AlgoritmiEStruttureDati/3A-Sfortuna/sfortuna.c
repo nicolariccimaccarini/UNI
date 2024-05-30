25eba77d4a8ea960df0040a3d793da9437530f63
nicolariccimaccarini <115147017+nicolariccimaccarini@users.noreply.github.com>
2024-05-30 10:25:42 +0200

    sfortuna.c

--- SecondoAnno/AlgoritmiEStruttureDati/3A-Sfortuna/sfortuna.c
+++ SecondoAnno/AlgoritmiEStruttureDati/3A-Sfortuna/sfortuna.c
@@ -1,29 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+
+#define MAX_N 10000
+
+// Struttura per rappresentare un nodo dell'albero di copertura minimo
+typedef struct 
+{
+    int v;
+    int key;
+} MinHeapNode;
 
 
-void sfortuna(FILE *in_file, FILE *out_file) {
-    int N, M;   // N = numero citta' --- M = proposte di strada
-
-    fscanf(in_file, "%d %d", &N, &M);
-}
-
-
-int main(void) {
-        // Apro i file di input e output
-    FILE *in_file = fopen("input.txt", "r");
-    FILE *out_file = fopen("output.txt", "w");
-
-    if (in_file == NULL || out_file == NULL) {
-        return -1;
-    }   
-
-    sfortuna(in_file, out_file);
-
-    // Chiudo i file di input e ourput
-    if (fclose(in_file) != 0 || fclose(out_file) != 0) {
-        return -2;
-    }
-
-    return 0;
-}
