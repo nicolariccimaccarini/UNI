dc36f9cf2f681970c67789b53331dfca9b73fa04
nicolariccimaccarini <115147017+nicolariccimaccarini@users.noreply.github.com>
2024-06-12 15:11:48 +0200

    sfortuna.c

--- SecondoAnno/Algoritmi_e_Strutture_Dati/Esercizi/3A-Sfortuna/sfortuna.c
+++ SecondoAnno/Algoritmi_e_Strutture_Dati/Esercizi/3A-Sfortuna/sfortuna.c
@@ -10,7 +10,7 @@ typedef struct {
 typedef struct {
     int from;               // nodo di partenza dell'arco
     int to;                 // nodo di destinazione dell'arco
-    long long weight;       // peso dell'arco
+    int weight;             // peso dell'arco
 } Edge;
 
 // Funzione per scambiare due valori interi
@@ -22,8 +22,8 @@ void swap(int *x, int *y) {
 
 // Funzione di comparazione per l'ordinamento degli archi in base al peso
 int weightComparator(const void *x, const void *y) {
-    long long w1 = ((const Edge *) x)->weight;
-    long long w2 = ((const Edge *) y)->weight;
+    int w1 = ((const Edge *) x)->weight;
+    int w2 = ((const Edge *) y)->weight;
     return w1 > w2;
 }
 
@@ -32,6 +32,7 @@ int findSet(int n, Set *sets) {
     while (n != sets[n].representative) {
         n = sets[n].representative;
     }
+
     return n;
 }
 
@@ -39,9 +40,13 @@ int findSet(int n, Set *sets) {
 void unionSet(int x, int y, Set *sets) {
     x = findSet(x, sets);
     y = findSet(y, sets);
+
     if (sets[x].size < sets[y].size) {
         swap(&x, &y);
     }
+
+    sets[x].size += sets[y].size;
+    sets[y].representative = x;
 }
 
 // Funzione per verificare se due nodi appartengono allo stesso set
@@ -50,9 +55,10 @@ bool isSameSet(int x, int y, Set *sets) {
 }
 
 // Funzione per calcolare il peso totale dell'albero di copertura minimo utilizzando l'algoritmo di Kruskal
-long long kruskal(Set *sets, Edge *edges, int nEdges) {
+int kruskal(Set *sets, Edge *edges, int nEdges) {
     Edge e;
-    long long sum = 0;
+    int sum = 0;
+
     for (int i = 0; i < nEdges; i++) {
         e = edges[i];
         if (!isSameSet(e.from, e.to, sets)) {
@@ -60,6 +66,7 @@ long long kruskal(Set *sets, Edge *edges, int nEdges) {
             sum += e.weight;
         }
     }
+
     return sum;
 }
 
@@ -70,8 +77,8 @@ void sfortuna(FILE *in_file, FILE *out_file) {
     // Leggo la prima riga dell'input
     fscanf(in_file, "%d %d", &N, &M);
 
-    Set *sets = malloc(N * sizeof(Set));
-    Edge *edges = malloc(M * sizeof(Edge));
+    Set *sets = (Set *) malloc(N * sizeof(Set));
+    Edge *edges = (Edge *) malloc(M * sizeof(Edge));
 
     // Inizializzo i set
     for (int i = 0; i < N; i++) {
@@ -87,7 +94,10 @@ void sfortuna(FILE *in_file, FILE *out_file) {
     // Ordino gli archi in base al peso
     qsort(edges, M, sizeof(Edge), weightComparator);
 
-    fprintf(out_file, "%lld\n", kruskal(sets, edges, M));
+    fprintf(out_file, "%d\n", kruskal(sets, edges, M));
+
+    free(sets);
+    free(edges);
 }
 
 int main(void) {
